d40194e4babdf27b260c2f6fd9508508eb8049b5
timazet <14309920+timazet@users.noreply.github.com>
2018-01-27 12:11:54 +0300

    Part01-Step03 was added

---
+++ part01/step03/src/test/java/com/timazet/SampleTest.java
@@ -0,0 +1,14 @@
+package com.timazet;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
